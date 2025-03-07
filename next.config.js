b6a75dc6799ace93f108a7ede8a2a1b7ed7a96a0
Duy Cao <thienduy1295@gmail.com>
2025-03-07 17:35:18 +0700

    adjust config next js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   experimental: {
     optimizePackageImports: ['@chakra-ui/react']
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/',
+          outputPath: 'static/',
+        },
+      },
+    })
+    return config
   }
 }
